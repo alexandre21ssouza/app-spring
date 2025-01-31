c7b2aab42c9452219433e3c0381ce857f7768191
alexandre21ssouza <alexandre2121ssouza@gmail.com>
2025-01-30 22:53:08 -0300

    Perfil de dev criado

--- src/main/java/com/projeto/aplicacao_spring_app/AplicacaoSpringAppApplication.java
+++ src/main/java/com/projeto/aplicacao_spring_app/AplicacaoSpringAppApplication.java
@@ -2,8 +2,10 @@ package com.projeto.aplicacao_spring_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
+@Profile("dev")
 public class AplicacaoSpringAppApplication {
 
 	public static void main(String[] args) {
