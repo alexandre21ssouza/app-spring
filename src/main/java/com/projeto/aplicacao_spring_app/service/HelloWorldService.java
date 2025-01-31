46f142c32affd8fc1bd7aec440b38b826805684d
alexandre21ssouza <alexandre2121ssouza@gmail.com>
2025-01-30 23:06:35 -0300

    Criação do Pacote service e suas classes

---
+++ src/main/java/com/projeto/aplicacao_spring_app/service/HelloWorldService.java
@@ -0,0 +1,11 @@
+package com.projeto.aplicacao_spring_app.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloWorldService {
+
+    public String helloWorld(){
+        return "hello Video!";
+    }
+}
